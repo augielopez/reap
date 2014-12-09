e886740a82d29a8bded413277823d958a81864f9
Patrick McLenithan <patrick.mclenithan@gmail.com>
2014-12-09 07:18:32 +0000

    DB sync

--- apps/www/app.js
+++ apps/www/app.js
@@ -62,9 +62,13 @@ app.use(function(err, req, res, next) {
 
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
+sequelize.sync({force: false}).success(function() {
+  console.log("DB synced successfully");
+  var server = app.listen(app.get('port'), function() {
+    debug('Express server listening on port ' + server.address().port);
+  });
+}).error(function(error) {
+  console.log("DB sync error: "+error);
 });
-
 
 module.exports = app;
